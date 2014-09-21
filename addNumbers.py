e0d98af413571d2dfe2f6ad4599a378bfec4eb7c
tongjiechen <tongjie.chen@gmail.com>
2014-09-20 18:26:38 -0700

    step 5

--- addNumbers.py
+++ addNumbers.py
@@ -7,8 +7,12 @@ def add(numbers, delimiter=","):
         if (numbers.startswith("//")):
             (delimiterline,numbers)=numbers.split("\n",2)
             delimiter = delimiterline.replace("/","")
-        numbers = numbers.replace("\n",",")
-        return sum(map(lambda x: int(x), numbers.split(delimiter)))
+        numbers = numbers.replace("\n",delimiter)
+        numbers_list = numbers.split(delimiter)
+        negative_numbers = filter(lambda x: int(x)<0, numbers_list)
+        if len(negative_numbers) > 0:
+            raise RuntimeError("negatives not allowed",negative_numbers)
+        return sum(map(lambda x: int(x), numbers_list))
 
 if __name__ == "__main__":
     
@@ -18,5 +22,5 @@ if __name__ == "__main__":
     print add("1,2,3,4")
     print add("1\n2,3")
     print add("//;\n1;2")
-
+    print add("-1,2,-3,4")
 
