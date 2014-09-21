546fbc256fb15167bad26d4e62c0ca80ca586981
tongjiechen <tongjie.chen@gmail.com>
2014-09-20 18:21:16 -0700

    step 4

--- addNumbers.py
+++ addNumbers.py
@@ -2,10 +2,13 @@
 
 def add(numbers, delimiter=","):
     if numbers == "":
-       return 0
+        return 0
     else :
-       numbers = numbers.replace("\n",",")
-       return sum(map(lambda x: int(x), numbers.split(delimiter)))
+        if (numbers.startswith("//")):
+            (delimiterline,numbers)=numbers.split("\n",2)
+            delimiter = delimiterline.replace("/","")
+        numbers = numbers.replace("\n",",")
+        return sum(map(lambda x: int(x), numbers.split(delimiter)))
 
 if __name__ == "__main__":
     
@@ -14,5 +17,6 @@ if __name__ == "__main__":
     print add("1,2")
     print add("1,2,3,4")
     print add("1\n2,3")
+    print add("//;\n1;2")
 
 
